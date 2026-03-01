67a0c9ca8e200b1aadd11a97f8d25c7f0be00532
João Vitor Martins <joaoescmartins@gmail.com>
2026-03-01 05:40:35 -0300

    feat: adicionadas classes Carro e Modifacacao

---
+++ src/main/java/projeto/jdm/garage/Carros/Carro.java
@@ -0,0 +1,33 @@
+package projeto.jdm.garage.Carros;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "carros")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Carro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String Marca;
+
+    @Column(nullable = false)
+    private String modelo;
+
+    @Column(nullable = false)
+    private int AnoFabricacao;
+
+    @Column(nullable = false)
+    private String Cor;
+
+}
---
+++ src/main/java/projeto/jdm/garage/Modificacoes/Modificacao.java
@@ -0,0 +1,30 @@
+package projeto.jdm.garage.Modificacoes;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table (name = "modificacoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Modificacao {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+
+    @Column(nullable = false)
+    private String nomePeca;
+
+    @Column(precision = 10, scale = 2)
+    private BigDecimal custo;
+
+    private String descricao;
+
+
+}
